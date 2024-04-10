9bd09244cfdd2218cb446bc1c03b4169501b0c65
junenu <junenu41@gmail.com>
2024-04-10 23:07:21 +0900

    update

---
+++ test.ts
@@ -0,0 +1,11 @@
+function Person(age) {
+    this.age = age;
+    var _this = this;  // capture this
+    this.growOld = function() {
+        _this.age++;   // use the captured this
+    }
+}
+var person = new Person(1);
+setTimeout(person.growOld,1000);
+
+setTimeout(function() { console.log(person.age); },2000); // 2
